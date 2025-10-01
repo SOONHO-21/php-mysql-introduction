a114ce815b220eadfb71a515999179995b6349a2
SOONHO-21 <ajsoon98@naver.com>
2025-10-01 15:38:05 +0900

    251001 Second

--- 13/www/include/header.php
+++ 13/www/include/header.php
@@ -1,25 +1,29 @@
 <?php
     session_start();
 
-    if (isset($_SESSION["userid"])) 
-        $userid = $_SESSION["userid"];
-    else {
-        $userid = "";
-    }
-    
-    if (isset($_SESSION["username"])) 
-        $username = $_SESSION["username"];
-    else 
-        $username = "";
-    
-    if (isset($_SESSION["userlevel"])) 
-        $userlevel = $_SESSION["userlevel"];
-    else 
-        $userlevel = "";
+    $userid = isset($_SESSION["userid"]) ? $_SESSION["userid"] : "";
+    $username = isset($_SESSION["username"]) ? $_SESSION["username"] : "";
+    $userlevel = isset($_SESSION["userlevel"]) ? $_SESSION["userlevel"] : "";
+
+    // if(isset($_SESSION["userid"]))
+    //     $userid = $_SESSION["userid"];
+    // else {
+    //     $userid = "";
+    // }
+
+    // if(isset($_SESSION["username"]))
+    //     $username = $_SESSION["username"];
+    // else
+    //     $username = "";
+
+    // if(isset($_SESSION["userlevel"]))
+    //     $username = $_SESSION["userlevel"];
+    // else
+    //     $userlevel = "";
 ?>
 <!DOCTYPE html>
-<html>
-<head> 
+<html lang="en">
+<head>
 <meta charset="utf-8">
 <title>PHP+MySQL 입문</title>
 <link rel="stylesheet" href="../css/style.css">
@@ -30,34 +34,36 @@
         <a href="../main/index.php">PHP+MySQL입문</a>
     </h3>
     <ul class="top_menu">
-<?php
-    if(!$userid) {
-        echo "<li>홈페이지에 오신 것을 환영합니다~~~ </li>";
-    }
-    else {
-        $logged = $username."(Level:".$userlevel.")님 환영합니다. ";
-        echo "<li>$logged</li>";
-    }
-?>
-    </ul> <!-- top_menu -->
+    <?php
+        if(!$userid) {
+            echo "<li>홈페이지에 오신 것을 환영합니다~~~ </li>";
+        } else {
+            $logged = $username."(Level:".$userlevel.")님 환영합니다. ";    //로그인 성공시 메시지
+            echo "<li>$logged</li>";
+        }
+    ?>
+    </ul>
 
     <ul class="main_menu">
-<?php
-    if(!$userid) {
-?>
-        <li><a href="../member/index.php?type=form">회원 가입</a> </li>
-        <li><a href="../member/index.php?type=login_form">로그인</a></li>
-<?php
-    } else {
-?>
-        <li><a href="../member/logout.php">로그아웃</a> </li>
-        <li><a href="../member/index.php?type=modify_form">정보 수정</a></li>
-<?php
-    }
-?>
+    <?php
+        if(!$userid) {
+    ?>
+    <li><a href="../member/index.php?type=form">회원 가입</a></li>
+    <li><a href="../member/index.php?type=login_form">로그인</a></li>
+    <?php
+        } else {
+    ?>
+    <li><a href="../member/logout.php">로그아웃</a></li>
+    <li><a href="../member/index.php?type=modify_form">정보 수정</a></li>
+    <li><a href="../member/profile.php">프로필</a></li>
+    <?php
+        }
+    ?>
     <li>|</li>
     <li><a href="../mboard/index.php?type=list&table=_notice">공지 게시판</a></li>
     <li><a href="../mboard/index.php?type=list&table=_qna">QNA 게시판</a></li>
     <li><a href="../mboard/index.php?type=list&table=_youtube">YOUTUBE 게시판</a></li>
     </ul>   <!-- main_menu -->
 </header>
+</body>
+</html>
